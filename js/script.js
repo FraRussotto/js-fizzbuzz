4900ffc18336e330ec51e000c6ce7dca5adc5543
FraRussotto <russottofrancesco1@gmail.com>
2023-09-11 16:29:39 +0200

    [FIX] if FizzBuzz

--- js/script.js
+++ js/script.js
@@ -4,16 +4,15 @@ for(let i = 1; i <=100; i++){
   box.classList.add("box", "m-3", "bg_default", "text_fr")
   box.append(i);
   
-  if((i % 3) === 0){
+  if(((i % 3) === 0) && ((i % 5) === 0)){
+    box.classList.add("bg_red")
+    box.innerHTML = "fizzbuzz";
+  } else if((i % 3) === 0){
     box.classList.add("bg_green")
     box.innerHTML = "fizz";
   } else if((i % 5) === 0){
     box.classList.add("bg_yellow")
     box.innerHTML = "buzz";
-  }
-  if(!(i % 3) && !(i % 5)){
-    box.classList.add("bg_red")
-    box.innerHTML = "fizzbuzz";
   }
   boxesContainer.append(box);
 }
