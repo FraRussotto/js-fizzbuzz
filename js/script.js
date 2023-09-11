12a8beca5b79e633fbffb34821f9fa2315c7e307
FraRussotto <russottofrancesco1@gmail.com>
2023-09-11 15:40:33 +0200

    [ADD] FizzBuzz

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ for(let i = 1; i <=100; i++){
   const box = document.createElement("div");
   box.classList.add("box", "m-3", "bg_default")
   box.append(i);
-
+  
   if((i % 3) === 0){
     box.classList.add("bg_green")
+    box.innerHTML = "fizz";
   } else if((i % 5) === 0){
     box.classList.add("bg_yellow")
+    box.innerHTML = "buzz";
   }
   if(((i % 3)+(i % 5)) === 0){
     box.classList.add("bg_red")
+    box.innerHTML = "fizzbuzz";
   }
   boxesContainer.append(box);
 }
